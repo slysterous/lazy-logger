1af1ef81b2a59b4937d5b0ab84e9f983091ba760
Χρήστος Πετρόπουλος < >
2017-06-04 22:43:56 +0300

    style(Folder Formatting): moved code to /src, add travis config, and semantic-release

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect=require('chai').expect;
+var lazyLogger=require('./index');
+
+describe('lazy-logger',function(){
+    it('should work!',function(){
+        expect(true).to.be.true;
+    })
+})
